47c1bbe9c30ace01e1687a0ebadf67598be53c47
Danilo Lemes <daniloleemes@hotmail.com>
2025-03-01 12:44:35 +0000

    connect more ui fields

--- Scripts/UiBindings/StatusWindowSourceScriptableObject.cs
+++ Scripts/UiBindings/StatusWindowSourceScriptableObject.cs
@@ -3,6 +3,27 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "StatusWindowSourceScriptableObject", menuName = "Scriptable Objects/StatusWindowSourceScriptableObject")]
 public class StatusWindowSourceScriptableObject : ScriptableObject
 {
+    public string Name;
+    public string JobName;
+    public long Hp;
+    public long MaxHp;
+    public long Sp;
+    public long MaxSp;
+    
+    public string CurrentHpPercent;
+    public string CurrentSpPercent;
+
+    public short BaseLevel;
+    public short JobLevel;
+    
+    public long BaseExp;
+    public long NextBaseExp;
+    public long JobExp;
+    public long NextJobExp;
+
+    public string Weight;
+    public long Money;
+    
     public string Str;
     public string Agi;
     public string Vit;
